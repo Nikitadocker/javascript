7ad72a6147567bc4348d58286982f8735caff347
nikita <nbarabanschikov@mts.ru>
2023-10-01 19:12:26 +0300

    add 2

--- switch/if.js
+++ switch/if.js
@@ -2,35 +2,37 @@
 // Напишите if..else, соответствующий следующему switch:
 // почему не запускается?
 
-// switch (browser) {
-//     case 'Edge':
-//         alert("You've got the Edge!");
-//         break;
 
-//     case 'Chrome':
-//     case 'Firefox':
-//     case 'Safari':
-//     case 'Opera':
-//         alert('Okay we support these browsers too');
-//         break;
+// let browser = prompt('Введите имя браузер', '');
+switch (browser) {
+    case 'Edge':
+        alert("You've got the Edge!");
+        break;
 
-//     default:
-//         alert('We hope that this page looks ok!');
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert('Okay we support these browsers too');
+        break;
+
+    default:
+        alert('We hope that this page looks ok!');
+}
+
+
+
+// let browser = prompt('Введите имя браузер', '');
+
+// if (browser == 'Edge') {
+//     alert("You've got the Edge!");
+// }
+// else if (browser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera') {
+//     alert('Okay we support these browsers too');
 // }
 
-
-
-let browser = prompt('Введите имя браузер', '');
-
-if (browser == 'Edge') {
-    alert("You've got the Edge!");
-}
-else if (browser == 'Chrome' || 'Firefox' || 'Safari' || 'Opera') {
-    alert('Okay we support these browsers too');
-}
-
-else {
-    alert('We hope that this page looks ok!');
-}
+// else {
+//     alert('We hope that this page looks ok!');
+// }
 
 
