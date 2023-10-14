e449075985e557b168113cd930c4f3d8d66d0870
nikita <nbarabanschikov@mts.ru>
2023-10-15 02:26:13 +0300

    add pow

--- function/pow.js
+++ function/pow.js
@@ -5,6 +5,27 @@
 // pow(1, 100) = 1 * 1 * ...* 1 = 1
 // Создайте страницу, которая запрашивает x и n, а затем выводит результат pow(x,n).
 
+// let a = prompt('Введите число  a');
+
+// let b = prompt('Введите число  b');
+
+
+// function min(a, b) {
+//     if (a > b) {
+//         return alert(`Число ${b} меньшее`);
+//     }
+
+//     else if (b > a) {
+//         return alert(`Число ${a} меньшее`);
+//     }
+
+//     else {
+//         return alert(`Числа равны. Выводим ${a}`);
+//     }
+// }
+
+// min(a, b);
+
 
 
 let x = prompt('Введите число  x');
@@ -13,9 +34,11 @@ let n = prompt('Введите cтепень  n');
 
 function pow(x, n) {
 
-    x ** n
+    x ** n == result
+
+    return alert (`Результ возведения числа в степень ${result}`);
 
 }
 
 
-pow(x, n)
+pow(x, n);
